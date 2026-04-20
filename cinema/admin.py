1084b812d8f24f64878781e41909b073feb9084b
Tanya <tanakutova74@gmail.com>
2026-04-20 22:38:33 +0300

    Solution

--- cinema/admin.py
+++ cinema/admin.py
@@ -10,10 +10,39 @@ from .models import (
     Ticket,
 )
 
-admin.site.register(CinemaHall)
-admin.site.register(Genre)
-admin.site.register(Actor)
-admin.site.register(Movie)
-admin.site.register(MovieSession)
+class DisableDeleteAdmin(admin.ModelAdmin):
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def get_actions(self, request):
+        actions = super().get_actions(request)
+        if "delete_selected" in actions:
+            del actions["delete_selected"]
+        return actions
+
+@admin.register(CinemaHall)
+class CinemaHallAdmin(DisableDeleteAdmin):
+    pass
+
+@admin.register(Genre)
+class GenreAdmin(DisableDeleteAdmin):
+    pass
+
+@admin.register(Actor)
+class ActorAdmin(DisableDeleteAdmin):
+    pass
+
+@admin.register(Movie)
+class MovieAdmin(DisableDeleteAdmin):
+    pass
+
+@admin.register(MovieSession)
+class MovieSessionAdmin(DisableDeleteAdmin):
+    pass
+
+@admin.register(Ticket)
+class TicketAdmin(DisableDeleteAdmin):
+    pass
+
 admin.site.register(Order)
-admin.site.register(Ticket)
+
